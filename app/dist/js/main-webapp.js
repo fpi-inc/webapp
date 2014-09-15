ed647c0fce8dd820337415c68449f6b07319424a
unknown <pingping_feng@user8084.fpihz.com>
2014-09-15 17:26:53 +0800

    122

--- app/dist/js/main-webapp.js
+++ app/dist/js/main-webapp.js
@@ -2,8 +2,8 @@
 
 angular.module('fpiwebapp.login.service', ['ngResource']).
 	factory('LoginService', ['$resource', function($resource) {
-		//return $resource('http://122.224.196.67:10001/mobile/mobile/load/login.do', {callback: 'JSON_CALLBACK'}, {
-		return $resource('http://172.19.20.218:8088/mobile/mobile/load/login.do', {callback: 'JSON_CALLBACK'}, {
+		return $resource('http://122.224.196.67:10001/mobile/mobile/load/login.do', {callback: 'JSON_CALLBACK'}, {
+		//return $resource('http://172.19.20.218:8088/mobile/mobile/load/login.do', {callback: 'JSON_CALLBACK'}, {
 		//return $resource('http://172.19.20.69:8088/mobile/mobile/load/login.do', {callback: 'JSON_CALLBACK'}, {
 			query: {method: 'JSONP'}
 		});
